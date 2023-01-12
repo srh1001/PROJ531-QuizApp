46939dabce66e19b9dc78cdbb0ab12285d09df5c
Hatimsadi <hatim.sadi@gmail.com>
2023-01-12 16:47:11 +0100

    nouveau nom

---
+++ quiz.py
@@ -0,0 +1,128 @@
+all_quizzes = {}
+import time
+class Quiz():
+    def __init__(self, name: str, questions: list, reponses: list[list], reponses_c: list[int], point: list[int]):
+        self.name = name
+        self.quest = questions
+        self.rep = reponses
+        self.rep_c = reponses_c
+        self.point = point
+
+    def __repr__(self):
+        return self.name
+    def calculate_time(self,t1,t2):
+        s=int(t2-t1)
+        h=0
+        m=0
+        if s//3600!=0:
+            h=s//3600
+            s=s%3600
+        if s%60!=0:
+            m=s//60
+            s=s%60
+        if h==0:
+            if m==0:
+                return f"{s} secondes"
+            else:
+                return f"{m} minutes {s} secondes"
+        else:
+            return f"{h} heures {m} minutes {s} secondes"
+
+    def launch_quiz(self):
+        t1=time.time()
+        score = 0
+        score_maximal = 0
+        print(self.name)
+        for i in range(len(self.quest)):
+            print()
+            print(f"Question {i+1} :", self.quest[i])
+            for j in range(len(self.rep[i])):
+                print(f"{j+1}) {self.rep[i][j]}", end=' ')
+            print()
+            while True:
+                try:
+                    reponse_u=int(input("choisir le numéro de votre réponse"))
+                except:
+                    print("saisir un nombre valide")
+                else:
+                    if reponse_u>len(self.rep[i]) or reponse_u<=0:
+                        print("saisir un nombre valide")
+                    else:
+                        break
+            if (reponse_u) == self.rep_c[i]:
+                score += self.point[i]
+                print(f"Bonne réponse, vous avez gagné {self.point[i]} points ")
+            else:
+                print(f"Mauvaise réponse , la réponse est {self.rep[i][int(self.rep_c[i])-1]}")
+            score_maximal += self.point[i]
+        t2=time.time()
+        print(f"Quiz terminé , votre score est {score}/{score_maximal} en {self.calculate_time(t1,t2)}")
+        return score
+
+
+def create_quiz_file(fichier: str):
+    '''
+    creation d une quiz a partir d un fichier
+    '''
+    global all_quizzes
+    file = open(fichier, "r")
+    lines = file.read().splitlines()
+    titre = lines[0]
+    question = lines[1].split(';')
+    rep = []
+    for k in range(2, len(lines) - 1):
+        rep.append(lines[k].split(';'))
+    point = lines[-1].split(';')
+    rep_c = []
+    p = []
+    for tu in point:
+        t = list(map(int, tu.split(',')))
+        rep_c.append(t[0])
+        p.append(t[1])
+    
+    all_quizzes[titre] = Quiz(titre, question, rep, rep_c, p)
+    return Quiz(titre, question, rep, rep_c, p)
+
+
+def create_quiz():
+    '''
+     creation d une quizz par l administrateur
+    '''
+    global all_quizzes
+    titre = input("Proposez un titre du quiz : ")
+    while True:
+        try:
+            n = int(input("Proposez le nombre de questions pour le quizz "))
+        except:
+            print("veuillez saisir un nombre")
+        else:
+            break
+    questions = []
+    reponses = []
+    rep_c = []
+    point = []
+    for i in range(n):
+        questions.append(input(f"Saisissez la question n°{i+1} "))
+        reponses.append(input("saisissez les réponses séparés par ; ").split(";"))
+        while True:
+            try:
+                rep_c.append(int(input("saisir le numéro de la réponse correcte ")) - 1)
+            except:
+                print("veuillez saisir un nombre")
+            else:
+                break
+        while True:
+            try:
+                point.append(int(input("saisir le score ")))
+            except:
+                print("veuillez saisir un nombre")
+            else:
+                break
+        print()
+    
+    all_quizzes[titre] = Quiz(titre, questions, reponses, rep_c, point)
+    return Quiz(titre, questions, reponses, rep_c, point)
+
+q=create_quiz_file('test.txt')
+q.launch_quiz()
+
