40f17fed9e854149110cf0acdd8bb913426666fd
unknown <sarahcadm@protonmail.com>
2023-01-13 10:01:26 +0100

    Import et export json, fichier class Quiz() et fichier pour créer les quiz sont maintenant séparés !

--- pro531.py
+++ pro531.py
@@ -1,66 +1,12 @@
-import time
+import os
+from quiz import *
+from savedatajson import *
 
-all_quizzes = {}
-
-class Quiz():
-    def __init__(self, name: str, questions: list, reponses: list[list], reponses_c: list[int], point: list[int]):
-        self.name = name
-        self.quest = questions
-        self.rep = reponses
-        self.rep_c = reponses_c
-        self.point = point
-
-    def __repr__(self):
-        return self.name
-    def calculate_time(self,t1,t2):
-        s=int(t2-t1)
-        h=0
-        m=0
-        if s//3600!=0:
-            h=s//3600
-            s=s%3600
-        if s%60!=0:
-            m=s//60
-            s=s%60
-        if h==0:
-            if m==0:
-                return f"{s} secondes"
-            else:
-                return f"{m} minutes {s} secondes"
-        else:
-            return f"{h} heures {m} minutes {s} secondes"
-
-    def launch_quiz(self):
-        t1=time.time()
-        score = 0
-        score_maximal = 0
-        print(self.name)
-        for i in range(len(self.quest)):
-            print()
-            print(f"Question {i+1} :", self.quest[i])
-            for j in range(len(self.rep[i])):
-                print(f"{j+1}) {self.rep[i][j]}", end=' ')
-            print()
-            while True:
-                try:
-                    reponse_u=int(input("choisir le numéro de votre réponse"))
-                except:
-                    print("saisir un nombre valide")
-                else:
-                    if reponse_u>len(self.rep[i]) or reponse_u<=0:
-                        print("saisir un nombre valide")
-                    else:
-                        break
-            if (reponse_u-1) == self.rep_c[i]:
-                score += self.point[i]
-                print(f"Bonne réponse, vous avez gagné {self.point[i]} points ")
-            else:
-                print(f"Mauvaise réponse , la réponse est {self.rep[i][int(self.rep_c[i])-1]}")
-            score_maximal += self.point[i]
-        t2=time.time()
-        print(f"Quiz terminé , votre score est {score}/{score_maximal} en {self.calculate_time(t1,t2)}")
-        return score
-
+if os.path.exists("C:/Users/srhmr/Downloads/all_test_json/data_all_quizzes.json"):
+    all_quizzes = extract_json_quiz("C:/Users/srhmr/Downloads/all_test_json/data_all_quizzes.json")
+else:
+    all_quizzes = {}
+print(all_quizzes)
 
 def create_quiz_file(fichier: str):
     '''
@@ -81,9 +27,12 @@ def create_quiz_file(fichier: str):
         t = list(map(int, tu.split(',')))
         rep_c.append(t[0])
         p.append(t[1])
-    
-    all_quizzes[titre] = Quiz(titre, question, rep, rep_c, p)
-    return Quiz(titre, question, rep, rep_c, p)
+
+    quiz = Quiz(titre, question, rep, rep_c, p)
+    all_quizzes[titre] = quiz
+    quiz_to_send_to_json(quiz)
+
+    return quiz
 
 
 def create_quiz():
@@ -122,8 +71,10 @@ def create_quiz():
                 break
         print()
     
-    all_quizzes[titre] = Quiz(titre, questions, reponses, rep_c, point)
-    return Quiz(titre, questions, reponses, rep_c, point)
+    quiz = Quiz(titre, questions, reponses, rep_c, point)
+    all_quizzes[titre] = quiz
+    quiz_to_send_to_json(quiz)
 
-create_quiz()
+    return quiz
 
+create_quiz_file("C:/Users/srhmr/OneDrive/Documents/USMB/Fi3/PROJ531/PROJ531-TP/PROJ531-QuizApp/admin_quiz0.txt")
